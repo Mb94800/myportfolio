43c74f0f2364ad8230e497ba43cf79ba15f6990d
Mohamed Boubaker <mb94800@gmail.com>
2017-06-19 22:49:37 +0200

    bug

---
+++ app/home/home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'home1Ctrl'
+  });
+}])
+
+.controller('home1Ctrl', [function() {
+
+}]);
